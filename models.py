4898f767618256fde55bc44d1a72770b541c61ea
jiwon kim <57991446+23jiwon@users.noreply.github.com>
2022-09-05 00:58:51 +0900

    Create models.py

---
+++ models.py
@@ -0,0 +1,9 @@
+# python 필기 파일과 같은 위치에 
+def linear_regression(data):
+    print("Linear Regression")
+
+def gaussian_mixture(data):
+    print("Gaussian Mixture")
+
+def random_forest(data):
+    print("Random Forest")
